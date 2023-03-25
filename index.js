9c710ed9e1daaa3dcc976099be9b36795db748e3
Abu Osama <112826965+Abu1Osama@users.noreply.github.com>
2023-03-25 20:44:16 +0530

    created

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const cors=require("cors")
 const app=express()
 app.use(cors())
 app.use(express.json())
+app.get("/",(req,res)=>{
+    res.send({"msg":"This is my Homepage"})
+})
 
 app.use("/user",userRouter)
 app.use(auth)
